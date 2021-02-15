42dd21a8a69ce7f83305ea9efbb1fb0027bcc8a1
Stanhoucke <stanhoucke@gmail.com>
2021-02-15 16:08:07 +0000

    rollercoaster implements isecurity

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        visitor1 = new Visitor(14, 1.2, 40.0);
+        visitor2 = new Visitor(19, 2.1, 40.0);
     }
 
     @Test
@@ -27,5 +32,15 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void isAllowedTo__ReturnsTrue(){
+        assertTrue(rollerCoaster.isAllowedTo(visitor2));
+    }
+
+    @Test
+    public void isAllowedTo__ReturnsFalse(){
+        assertFalse(rollerCoaster.isAllowedTo(visitor1));
     }
 }
